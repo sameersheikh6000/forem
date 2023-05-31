806c78204ae50e3e8de3a7f935082befaa03482f
sameer <sameersheikh6000@gmail.com>
2023-05-31 22:06:36 +0500

    fix issue

--- app/decorators/lib/forem/user_class_decorator.rb
+++ app/decorators/lib/forem/user_class_decorator.rb
@@ -1,2 +1,15 @@
+module Lib
+    module Forem
+        module UserClassDecorator
+            def self.included(base)
+            end
+        end
+    end
+end
+  
+Forem.user_class.include(Lib::Forem::UserClassDecorator) if Forem.user_class
+  
+
+
 # Fix for #88
-Forem.decorate_user_class! if Forem.user_class
+# Forem.decorate_user_class! if Forem.user_class
