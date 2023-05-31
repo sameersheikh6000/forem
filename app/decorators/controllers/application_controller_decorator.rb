890dc26648289a37db9b95e7865ca47be69d0b50
sameer <sameersheikh6000@gmail.com>
2023-05-31 21:55:36 +0500

    fix issue

--- app/decorators/controllers/application_controller_decorator.rb
+++ app/decorators/controllers/application_controller_decorator.rb
@@ -1,3 +1,15 @@
-ApplicationController.class_eval do
-  helper Forem::Engine.helpers
+module Controllers
+  module ApplicationControllerDecorator
+    def self.prepended(base)
+      base.class_eval do
+        helper Forem::Engine.helpers
+      end
+    end
+  end
 end
+
+ApplicationController.prepend(Controllers::ApplicationControllerDecorator)
+
+# ApplicationController.class_eval do
+#   helper Forem::Engine.helpers
+# end
